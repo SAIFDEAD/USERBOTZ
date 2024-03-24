2cbbe15a7ae927934107793b13121df67580c350
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-24 06:19:00 +0530

    ⚡ 𝐒αιғβσтƨ ⚡

--- updater.py
+++ updater.py
@@ -52,3 +52,6 @@ async def update_requirements(main , test):
 # loop.run_until_complete(update_requirements(sys.argv[1] , sys.argv[2]))
 # loop.close()
 asyncio.run(update_requirements(sys.argv[1] , sys.argv[2]))
+
+
+# SAIFDEAD
